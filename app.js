16d4462fa7b8da8d36d85b0df65bd05b6580cfca
Yelena <88336329+DYA13@users.noreply.github.com>
2022-12-31 22:13:03 +0100

    message

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ downBtn.addEventListener('click',()=> {
 changeSlide('down')
 })
 
+document.addEventListener('keydown',
+event => {
+if (event.key === 'ArrowUp') {
+changeSlide('up')
+}
+else if (event.key === 'ArrowDown' ){
+    changeSlide('down')
+}
+})
+
 function changeSlide(direction) {
 if (direction === 'up') {
 activeSlideIndex++
